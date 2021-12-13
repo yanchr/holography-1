86ff3b58af0baf38c06fdbb7a84da023e01c6ca4
Christen, Yanick-MGB-M0365530 <yanick.christen@mgb.ch>
2021-12-13 22:08:59 +0100

    first commit

---
+++ src/models.js
@@ -0,0 +1,71 @@
+import * as THREE from 'three'
+import { DoubleSide } from 'three'
+import {getElapsedTime} from './script'
+
+
+export class Models {
+
+
+    mainMaterial() {
+        return new THREE.MeshBasicMaterial({ color: 0x4d8aeb })
+    }
+
+    material(color) {
+        return new THREE.MeshBasicMaterial({ color })
+    }
+
+    addToScene(scene) {
+       // scene.add(new THREE.AxesHelper(5))
+        scene.add(this.getLine(Math.PI / 4), this.getLine(-Math.PI / 4))
+        
+    }
+
+    setMirroredBox(object) {
+        const object1 = object.clone()
+        const object2 = object.clone()
+        const object3 = object.clone()
+        const object4 = object.clone()
+        object1.position.x = 10
+        object2.position.y = 10
+        object3.position.x = -10
+        object4.position.y = -10
+        this.rotateObject(object1)
+        this.rotateObject(object2)
+        this.rotateObject(object3)
+        this.rotateObject(object4)
+        return [object1, object2, object3, object4]
+       
+    }
+
+    rotateObject(object) {
+        const clock = new THREE.Clock()
+        const tick = () => {
+            const elapsedTime = clock.getElapsedTime()
+            object.rotation.y = - elapsedTime
+
+            // Call tick again on the next frame
+            window.requestAnimationFrame(tick)
+        }
+
+        tick()
+    }
+
+
+    getBox() {
+        const object = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 5, 1), this.material(0xff0000))
+        return object
+    }
+
+    getTorus(radius, dicke) {
+        const object = new THREE.Mesh(new THREE.TorusGeometry( radius, dicke, 16, 100 ), this.material(0xff0000))
+        return object
+    }
+
+    getLine(rotation) {
+        const size = 0.5
+        const length = 30
+        const object = new THREE.Mesh(new THREE.CylinderGeometry(size, size, length, 32), this.mainMaterial())
+        object.rotation.z = rotation
+        return object
+    }
+}
---
+++ src/script.js
@@ -0,0 +1,117 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import gsap from 'gsap'
+import * as dat from 'dat.gui'
+import { Models } from './models'
+
+
+/**
+ * Initialize
+ */
+const models = new Models()
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Object
+ */
+//scene.add(mesh)
+models.addToScene(scene)
+
+const mirrorObjects = [
+    models.getBox(), models.getTorus(3, 1)
+]
+let mirrorIndex = 0 
+let meshes = models.setMirroredBox(mirrorObjects[mirrorIndex])
+
+meshes.forEach(mesh => scene.add(mesh))
+/**
+* Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('click', () => 
+{
+    if(mirrorIndex < mirrorObjects.length - 1) {
+        mirrorIndex++
+        console.log('plus')
+    } else {
+        mirrorIndex = 0
+        console.log('no' +  mirrorObjects.length)
+    }
+    meshes.forEach(mesh => scene.remove(mesh))
+    meshes = models.setMirroredBox(mirrorObjects[mirrorIndex])
+    meshes.forEach(mesh => scene.add(mesh))
+})
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 20
+scene.add(camera)
+
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
+
+export function getElapsedTime() {
+    console.log("ddd")
+}
